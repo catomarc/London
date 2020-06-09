f0ce0e34eb10d0ba64c0c7e5d16d36a7c30176b1
catomarc <66695109+catomarc@users.noreply.github.com>
2020-06-09 19:39:27 +0100

    Add files via upload

---
+++ activities.js
@@ -0,0 +1,11 @@
+function myFunction() {
+
+  var email;
+  var email = prompt("Please enter your email", "@gmail.com")
+  {
+    txt = "Thank you, check your inbox for a gift.";
+  } 
+
+
+  document.getElementById("email").innerHTML = txt;
+}
